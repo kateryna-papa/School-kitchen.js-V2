01ce8832517e98fbf9d130ce09780c36e13e550a
kateryna-papa <91078377+kateryna-papa@users.noreply.github.com>
2023-05-08 22:05:42 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -217,7 +217,7 @@ async function fetchMenu() {
   foodsList.innerHTML = "";
   loaderMenu.classList.add('show');
   await fetch(
-      "https://school-kitchen-b274e-default-rtdb.firebaseio.com/menu.json"
+      "https://school-kitchen-71370-default-rtdb.firebaseio.com/menu.json"
   )
       .then((response) => {
         return response.json();
@@ -545,7 +545,7 @@ function sendOrdersToFireBase(obj) {
     }
   }
   const apiService = new ApiService(
-    "https://school-kitchen-b274e-default-rtdb.firebaseio.com"
+    "https://school-kitchen-71370-default-rtdb.firebaseio.com"
   );
 
   apiService.createOrder(obj);
